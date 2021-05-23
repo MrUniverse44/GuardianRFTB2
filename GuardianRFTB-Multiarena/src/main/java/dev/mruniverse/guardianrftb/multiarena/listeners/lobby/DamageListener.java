9860f00e1a85157fe51a2f9003abdea311df05ed
MrUniverse44 <oficial.mruniverse@gmail.com>
2021-05-23 13:23:43 -0500

    Improvements

--- GuardianRFTB-Multiarena/src/main/java/dev/mruniverse/guardianrftb/multiarena/listeners/lobby/DamageListener.java
+++ GuardianRFTB-Multiarena/src/main/java/dev/mruniverse/guardianrftb/multiarena/listeners/lobby/DamageListener.java
@@ -1,7 +1,6 @@
 package dev.mruniverse.guardianrftb.multiarena.listeners.lobby;
 
 import dev.mruniverse.guardianrftb.multiarena.GuardianRFTB;
-import dev.mruniverse.guardianrftb.multiarena.enums.GuardianBoard;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -25,17 +24,6 @@ public class DamageListener implements Listener {
                 if(plugin.getSettings().getSettings().getBoolean("settings.lobby.voidSpawnTP")) {
                     if (event.getCause().equals(EntityDamageEvent.DamageCause.VOID)) {
                         event.getEntity().teleport(lobby);
-                    }
-                }
-            }
-            if(plugin.getPlayerData(player.getUniqueId()).getGame() != null) {
-                GuardianBoard board = plugin.getPlayerData(player.getUniqueId()).getBoard();
-                if(board.equals(GuardianBoard.WAITING) || board.equals(GuardianBoard.BEAST_SPAWN) || board.equals(GuardianBoard.SELECTING) || board.equals(GuardianBoard.STARTING) || board.equals(GuardianBoard.WIN_BEAST_FOR_BEAST) || board.equals(GuardianBoard.WIN_BEAST_FOR_RUNNERS) || board.equals(GuardianBoard.WIN_RUNNERS_FOR_BEAST) || board.equals(GuardianBoard.WIN_RUNNERS_FOR_RUNNERS)) {
-                    if(event.getCause() == EntityDamageEvent.DamageCause.VOID) player.teleport(lobby);
-                    event.setCancelled(true);
-                } else {
-                    if(event.getCause().equals(EntityDamageEvent.DamageCause.FALL) || plugin.getPlayerData(player.getUniqueId()).getGame().getSpectators().contains(player)) {
-                        event.setCancelled(true);
                     }
                 }
             }
