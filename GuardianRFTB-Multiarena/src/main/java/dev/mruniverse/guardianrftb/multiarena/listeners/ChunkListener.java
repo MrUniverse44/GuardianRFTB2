1155bbd3ed617de38fadce96ca26ff303cdbef06
MrUniverse44 <oficial.mruniverse@gmail.com>
2021-07-26 00:42:18 -0500

    Added cancel-chunk-unload option

--- GuardianRFTB-Multiarena/src/main/java/dev/mruniverse/guardianrftb/multiarena/listeners/ChunkListener.java
+++ GuardianRFTB-Multiarena/src/main/java/dev/mruniverse/guardianrftb/multiarena/listeners/ChunkListener.java
@@ -2,11 +2,11 @@ package dev.mruniverse.guardianrftb.multiarena.listeners;
 
 import dev.mruniverse.guardianrftb.multiarena.GuardianRFTB;
 import dev.mruniverse.guardianrftb.multiarena.enums.GuardianFiles;
+import dev.mruniverse.guardianlib.core.events.GuardianChunkUnloadEvent;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.world.ChunkUnloadEvent;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -17,24 +17,24 @@ public class ChunkListener implements Listener {
 
     public ChunkListener(GuardianRFTB plugin) {
         this.plugin = plugin;
-        cancelUnload = plugin.getStorage().getControl(GuardianFiles.SETTINGS).getBoolean("settings.cancel-chunk-unload");
+        cancelUnload = plugin.getStorage().getControl(GuardianFiles.SETTINGS).getBoolean("settings.cancel-chunk-unload",true);
     }
 
     public void updateAll() {
-        cancelUnload = plugin.getStorage().getControl(GuardianFiles.SETTINGS).getBoolean("settings.cancel-chunk-unload");
+        cancelUnload = plugin.getStorage().getControl(GuardianFiles.SETTINGS).getBoolean("settings.cancel-chunk-unload",true);
     }
-    /*
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onChunkUnload(ChunkUnloadEvent event) {
-        Set<World> worlds = new HashSet<>(plugin.getGameManager().getGameWorlds().keySet());
-        if(plugin.getSettings().getLocation() != null) worlds.add(plugin.getSettings().getLocation().getWorld());
-        if(worlds.contains(event.getWorld())) {
 
-             GUARDIAN LIB
-             plugin.getLib().getNMS().cancelUnloadChunk();
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onChunkUnload(GuardianChunkUnloadEvent event) {
+        if(cancelUnload) {
+            Set<World> worlds = new HashSet<>(plugin.getGameManager().getGameWorlds().keySet());
+            if (plugin.getSettings().getLocation() != null) worlds.add(plugin.getSettings().getLocation().getWorld());
+            if (worlds.contains(event.getWorld())) {
+                event.setCancelled(true);
+            }
         }
     }
-    */
+
 
 
     //settings.cancel-chunk-unload
